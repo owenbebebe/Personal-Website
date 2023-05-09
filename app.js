3cef7ca4779c55a31880360a44054d5de0766795
Owen Li <87036763+owenbebebe@users.noreply.github.com>
2023-05-09 17:28:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ function animate() {
     ctxH.clearRect(0, 0, canvasH.width, canvasH.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //back curve
-    for(let j = 70; j >= 1; j--) {
+    for(let j = 80; j >= 1; j--) {
         ctxH.beginPath();
         ctxH.moveTo(0, (canvasH.height / 2) * j);
         for (let i = 0; i < canvasH.width; i += 5) {
             let y =  canvasH.height / 2 + Math.cos(i * 0.0057+ time) * (j*0.03) * 50 ;
-            ctxH.lineTo(i, 0.8*y+(j*-5));
+            ctxH.lineTo(i, 0.8*y+(j*-4));
         }
-        ctxH.strokeStyle = "#454442";
+        ctxH.strokeStyle = "#c5947e";
         ctxH.globalAlpha = 0.0142 * j;
         ctxH.stroke();
 
@@ -38,7 +38,7 @@ function animate() {
             let y = canvas.height / 2 + Math.sin(i * 0.01+ timeLow) * (j*0.085) * 50 ;
             ctx.lineTo(i, y+(j*-5));
         }
-        ctx.strokeStyle = "#454442";
+        ctx.strokeStyle = "#ff793c";
         // ctx.fillStyle= "#FEFBFA";
         ctx.globalAlpha = (0.03 * j);
         ctx.stroke();
@@ -73,8 +73,9 @@ function draw() {
     for(let i = 1; i <= 10; i++) {
         ctxM.beginPath();
         ctxM.arc((canvasM.width*(1+scrollTop*-0.00066)/1.15), (canvasM.height*moonY)/10, 2*  radius *  i * (1-moonR), 0, Math.PI * 2);
-        ctxM.lineWidth = radius * 0.01;
+        ctxM.lineWidth = radius * 0.03;
         ctxM.globalAlpha = (1+scrollTop*-0.0041117)* (1- (0.04997 * i));
+        ctxM.strokeStyle = "#ff793c";
         ctxM.stroke();
     }
     
@@ -94,9 +95,9 @@ function updateHeaderPosition() {
         let scrollOpc = 1-(currentScrollPos*0.00333);
         scrollNav.style.opacity = scrollOpc;
         scrollHero.style.opacity = scrollOpc;
-        canvasH.style.transform = `translateY(${currentScrollPos*0.889}px)`;
-        canvas.style.transform = `translateY(${currentScrollPos*0.667}px)`;        
-        
+        canvasH.style.transform = `translateY(${currentScrollPos*0.667}px)`;
+        canvas.style.transform = `translateY(${currentScrollPos*0.9}px)`;        
+        //somebody once told me the world is gonna roll me I aint the sharpest tool in the sheded
     }
     if (currentScrollPos >= 300) {
         scrollNav.style.opacity = 0;
@@ -130,3 +131,42 @@ const hiddenElement = document.querySelectorAll('.hidden');
 hiddenElement.forEach((el) => observer.observe(el));
 
 //scrolling hero page
+
+//drag card function
+
+const photoCon = document.querySelector(".photo-container");
+
+photoCon.Left = 150;
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function (e) {
+    pos = {
+        // The current scroll
+        left: photoCon.scrollLeft,
+        top: photoCon.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+    };
+    photoCon.style.cursor = 'grabbing';
+    photoCon.style.userSelect = 'none';
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    photoCon.scrollTop = pos.top - dy;
+    photoCon.scrollLeft = pos.left - dx;
+};
