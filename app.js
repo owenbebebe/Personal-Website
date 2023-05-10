e3962bceb2f2917b68488e6db18ed71e129e731e
Owen Li <87036763+owenbebebe@users.noreply.github.com>
2023-05-10 04:04:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -15,19 +15,6 @@ function animate() {
     time += 0.008;
     ctxH.clearRect(0, 0, canvasH.width, canvasH.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //back curve
-    for(let j = 80; j >= 1; j--) {
-        ctxH.beginPath();
-        ctxH.moveTo(0, (canvasH.height / 2) * j);
-        for (let i = 0; i < canvasH.width; i += 5) {
-            let y =  canvasH.height / 2 + Math.cos(i * 0.0057+ time) * (j*0.03) * 50 ;
-            ctxH.lineTo(i, 0.8*y+(j*-4));
-        }
-        ctxH.strokeStyle = "#c5947e";
-        ctxH.globalAlpha = 0.0142 * j;
-        ctxH.stroke();
-
-    } 
     //front  curve
     
     timeLow += 0.03;
@@ -42,7 +29,22 @@ function animate() {
         // ctx.fillStyle= "#FEFBFA";
         ctx.globalAlpha = (0.03 * j);
         ctx.stroke();
-    }    
+    }   
+    //back curve
+    for(let j = 80; j >= 1; j--) {
+        ctxH.beginPath();
+        ctxH.moveTo(0, (canvasH.height / 2) * j);
+        for (let i = 0; i < canvasH.width; i += 5) {
+            let y =  canvasH.height / 2 + Math.cos(i * 0.0057+ time) * (j*0.03) * 50 ;
+            ctxH.lineTo(i, 0.8*y+(j*-4));
+        }
+        ctxH.strokeStyle = "#c5947e";
+        ctxH.globalAlpha = 0.0142 * j;
+        ctxH.fillStyle = "#FEFBFA";
+        ctxH.fill();
+        ctxH.stroke();
+
+    }  
   }
   
   animate();
@@ -65,19 +67,28 @@ function setSize() {
 
 function draw() {
     let scrollTop = window.scrollY || window.pageYOffset;
-    let moonY = 1;
-    let moonX = 1;
-    moonY = 1 + (0.03777 *scrollTop);
-    let moonR = 1/(1+Math.exp(-0.00333*scrollTop));
-    ctxM.clearRect(0, 0, canvasM.width, canvasM.height);
-    for(let i = 1; i <= 10; i++) {
-        ctxM.beginPath();
-        ctxM.arc((canvasM.width*(1+scrollTop*-0.00066)/1.15), (canvasM.height*moonY)/10, 2*  radius *  i * (1-moonR), 0, Math.PI * 2);
-        ctxM.lineWidth = radius * 0.03;
-        ctxM.globalAlpha = (1+scrollTop*-0.0041117)* (1- (0.04997 * i));
-        ctxM.strokeStyle = "#ff793c";
-        ctxM.stroke();
+    console.log(scrollTop);
+    if(scrollTop <= 150) {
+        canvasM.style.opacity = "1";
+        let moonY = 1;
+        let moonX = 1;
+        moonY = 1 + (0.03777 *scrollTop);
+        let moonR = 1/(1+Math.exp(-0.00333*scrollTop));
+        ctxM.clearRect(0, 0, canvasM.width, canvasM.height);
+        for(let i = 1; i <= 10; i++) {
+            ctxM.beginPath();
+            ctxM.arc((canvasM.width*(1+scrollTop*-0.00066)/1.15), (canvasM.height*moonY)/10, 2*  radius *  i * (1-moonR), 0, Math.PI * 2);
+            canvasM.style.transform = `translateY(${scrollTop}px)`;
+            ctxM.lineWidth = radius * 0.02;
+            ctxM.fillStyle = "#ff793c";
+            ctxM.fill();
+            ctxM.globalAlpha = (1+scrollTop*-0.006667)* (1- (0.17 * i));
+        }
     }
+    else {
+        canvasM.style.opacity = "0";
+    }
+    
     
 }
 
@@ -131,33 +142,37 @@ hiddenElement.forEach((el) => observer.observe(el));
 
 //scrolling hero page
 
-//drag card function
-const container = document.querySelector('.photo-container');
-let isDown = false;
-let startX;
-let scrollLeft;
+//drag photos function
+// const photoContainer = document.querySelector('.photo-container');
+// const photos = document.querySelector('.photo');
 
-container.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-  container.classList.add('active');
-});
 
-container.addEventListener('mouseleave', () => {
-  isDown = false;
-  container.classList.remove('active');
-});
 
-container.addEventListener('mouseup', () => {
-  isDown = false;
-  container.classList.remove('active');
-});
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-container.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 3; // adjust the speed of scrolling here
-  container.scrollLeft = scrollLeft - walk;
-});
+// photoContainer.addEventListener('mousedown', (e) => {
+//   isDown = true;
+//   startX = e.pageX - photoContainer.offsetLeft;
+//   scrollLeft = photoContainer.scrollLeft;
+//   photoContainer.classList.add('active');
+// });
+
+// photoContainer.addEventListener('mouseleave', () => {
+//   isDown = false;
+//   photoContainer.classList.remove('active');
+// });
+
+// photoContainer.addEventListener('mouseup', () => {
+//   isDown = false;
+//   photoContainer.classList.remove('active');
+// });
+
+// photoContainer.addEventListener('mousemove', (e) => {
+//   if (!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - photoContainer.offsetLeft;
+//   const walk = (x - startX) * 3; // adjust the speed of scrolling here
+//   photoContainer.scrollLeft = scrollLeft - walk;
+// });
